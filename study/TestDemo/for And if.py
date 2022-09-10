d41cde0eaae298e6cd45d54079b71f00c40c113f
wulx <wulx>
2022-09-11 00:23:12 +0800

    for 循环的学习

---
+++ study/TestDemo/for And if.py
@@ -0,0 +1,9 @@
+# -*- coding:utf-8 -*-
+for i in range(1,10):
+    print(i , "",end="")
+print()
+dict1 = {"wulx":12,"wulx2":13}
+for i in dict1:
+    print(i)
+for key in dict1.items():
+    print(key)
