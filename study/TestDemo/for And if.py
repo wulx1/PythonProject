4e0573fd4ab5d26616b9e19ab94e50616751ccd7
wulx <wulx>
2022-09-11 15:53:21 +0800

    for 循环的学习

--- study/TestDemo/for And if.py
+++ study/TestDemo/for And if.py
@@ -9,3 +9,14 @@ for key in dict1.items():
     print(key)
 for i in dict1.values():
     print(i)
+
+#当我们需要判断 sum 大于 1000 的时候，不在相加时，可以用到 break ，退出整个循环
+count = 1
+sum = 0
+while(count<100):
+    sum = sum + count
+    if sum>1000:
+        break
+    count = count +1
+print(sum)
+
